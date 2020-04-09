6fb6dfe35e2702259e46c76e6929f4d15ad1cb73
Steffen Sauder <ssauder@pro-vision.de>
2020-04-09 18:22:57 +0200

    reformatted WaitForServerStartupExtension

--- integration-test/launchpad-test/src/test/java/io/wcm/caravan/hal/it/extensions/WaitForServerStartupExtension.java
+++ integration-test/launchpad-test/src/test/java/io/wcm/caravan/hal/it/extensions/WaitForServerStartupExtension.java
@@ -28,61 +28,63 @@ import io.wcm.caravan.hal.resource.HalResource;
 
 public class WaitForServerStartupExtension implements BeforeAllCallback {
 
-	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-	private static final JsonFactory JSON_FACTORY = new JsonFactory(OBJECT_MAPPER);
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private static final JsonFactory JSON_FACTORY = new JsonFactory(OBJECT_MAPPER);
 
-	@Override
-	public void beforeAll(ExtensionContext context) throws Exception {
+  @Override
+  public void beforeAll(ExtensionContext context) throws Exception {
 
-		int maxWaitSeconds = 30;
-		Stopwatch sw = Stopwatch.createStarted();
-		while (sw.elapsed(TimeUnit.SECONDS) < maxWaitSeconds) {
-			try {
-				assertHalResourceFoundAt(SERVICE_ID);
-				return;
-			} catch (Exception e) {
-				System.err.println("Caught " + e.getClass().getSimpleName()
-						+ " waiting for server to start up before starting tests, will try again...");
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		}
-		throw new IllegalStateException("Entry point at " + TestEnvironmentConstants.SERVER_URL + SERVICE_ID
-				+ " still fails to load after waiting " + maxWaitSeconds + " seconds");
-	}
+    int maxWaitSeconds = 30;
+    Stopwatch sw = Stopwatch.createStarted();
+    while (sw.elapsed(TimeUnit.SECONDS) < maxWaitSeconds) {
+      try {
+        assertHalResourceFoundAt(SERVICE_ID);
+        return;
+      }
+      catch (Exception e) {
+        System.err.println("Caught " + e.getClass().getSimpleName()
+            + " waiting for server to start up before starting tests, will try again...");
+        try {
+          Thread.sleep(1000);
+        }
+        catch (InterruptedException e1) {
+          // TODO Auto-generated catch block
+          e1.printStackTrace();
+        }
+      }
+    }
+    throw new IllegalStateException("Entry point at " + TestEnvironmentConstants.SERVER_URL + SERVICE_ID
+        + " still fails to load after waiting " + maxWaitSeconds + " seconds");
+  }
 
-	private static HalResource assertHalResourceFoundAt(String url) throws IOException {
+  private static HalResource assertHalResourceFoundAt(String url) throws IOException {
 
-		String fullUrl = SERVER_URL + url;
-		HttpResponse response = getResponse(fullUrl);
+    String fullUrl = SERVER_URL + url;
+    HttpResponse response = getResponse(fullUrl);
 
-		assertThat(response.getStatusLine().getStatusCode()).as("Response code for " + fullUrl)
-				.isEqualTo(HttpServletResponse.SC_OK);
+    assertThat(response.getStatusLine().getStatusCode()).as("Response code for " + fullUrl)
+        .isEqualTo(HttpServletResponse.SC_OK);
 
-		assertThat(response.getFirstHeader("Content-Type").getValue()).as("Content type for " + fullUrl)
-				.isEqualTo(HalResource.CONTENT_TYPE);
+    assertThat(response.getFirstHeader("Content-Type").getValue()).as("Content type for " + fullUrl)
+        .isEqualTo(HalResource.CONTENT_TYPE);
 
-		String jsonString = EntityUtils.toString(response.getEntity());
-		assertThat(jsonString).as("JSON response").isNotBlank();
+    String jsonString = EntityUtils.toString(response.getEntity());
+    assertThat(jsonString).as("JSON response").isNotBlank();
 
-		JsonNode json = JSON_FACTORY.createParser(jsonString).readValueAsTree();
-		HalResource halResource = new HalResource(json);
+    JsonNode json = JSON_FACTORY.createParser(jsonString).readValueAsTree();
+    HalResource halResource = new HalResource(json);
 
-		assertThat(halResource.getLink()).as("self link").isNotNull();
-		assertThat(halResource.getLink().getHref()).as("self URL").startsWith(url);
+    assertThat(halResource.getLink()).as("self link").isNotNull();
+    assertThat(halResource.getLink().getHref()).as("self URL").startsWith(url);
 
-		return halResource;
-	}
+    return halResource;
+  }
 
-	private static HttpResponse getResponse(String fullUrl) throws IOException, ClientProtocolException {
-		HttpGet get = new HttpGet(fullUrl);
+  private static HttpResponse getResponse(String fullUrl) throws IOException, ClientProtocolException {
+    HttpGet get = new HttpGet(fullUrl);
 
-		CloseableHttpClient client = HttpClientBuilder.create().build();
-		return client.execute(get);
-	}
+    CloseableHttpClient client = HttpClientBuilder.create().build();
+    return client.execute(get);
+  }
 
 }
