6aa53afe7a29026ef0f646bb5bf914524b3e21fd
Steffen Sauder <ssauder@pro-vision.de>
2020-08-16 19:36:05 +0200

    added some more useful standard relations

--- reha/api-interfaces/src/main/java/io/wcm/caravan/reha/api/relations/StandardRelations.java
+++ reha/api-interfaces/src/main/java/io/wcm/caravan/reha/api/relations/StandardRelations.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,6 +33,12 @@ public final class StandardRelations {
   }
 
   /**
+   * Refers to a resource that is the subject of the link's context.
+   * @see <a href="https://www.iana.org/go/rfc6903">RFC6903, section 2</a>
+   */
+  public static final String ABOUT = "about";
+
+  /**
    * Refers to a substitute for this context
    * @see <a href="http://www.w3.org/TR/html5/links.html#link-type-alternate">HTML5 link-type-alternate</a>
    */
@@ -49,6 +55,12 @@ public final class StandardRelations {
    * @see <a href="http://www.iana.org/go/rfc6573">RFC6573</a>
    */
   public static final String COLLECTION = "collection";
+
+  /**
+   * Refers to a resource that is not part of the same site as the current context.
+   * @see <a href="https://html.spec.whatwg.org/multipage/links.html#link-type-external">HTML Spec</a>
+   */
+  public static final String EXTERNAL = "external";
 
   /**
    * An IRI that refers to the furthest preceding resource in a series of resources.
@@ -93,7 +105,14 @@ public final class StandardRelations {
   public static final String RELATED = "related";
 
   /**
-   * section Refers to a section in a collection of resources.
+   * Refers to a resource that can be used to search through the link's context and related resources. @see
+   * <a href="http://www.opensearch.org/Specifications/OpenSearch/1.1">OpenSearch</a>
+   */
+  public static final String SEARCH = "search";
+
+  /**
+   * Refers to a section in a collection of resources.
+   * @see <a href="https://www.w3.org/TR/html401/">HTML 4.01 Specification</a>
    */
   public static final String SECTION = "section";
 
@@ -102,6 +121,12 @@ public final class StandardRelations {
    * @see <a href="http://www.iana.org/go/rfc4287">RFC4287</a>
    */
   public static final String SELF = "self";
+
+  /**
+   * Refers to a resource serving as a subsection in a collection of resources.
+   * @see <a href="https://www.w3.org/TR/html401/">HTML 4.01 Specification</a>
+   */
+  public static final String SUBSECTION = "subsection";
 
   /**
    * Refers to a parent document in a hierarchy of documents.
