6982c3a0822e1c0e161a5a58258f1d755cedd622
Steffen Sauder <ssauder@pro-vision.de>
2021-04-05 12:22:40 +0200

    updated javadocs

--- api-interfaces/src/main/java/io/wcm/caravan/rhyme/api/resources/LinkableResource.java
+++ api-interfaces/src/main/java/io/wcm/caravan/rhyme/api/resources/LinkableResource.java
@@ -36,7 +36,7 @@ public interface LinkableResource {
    * Create a link to this resource, including meaningful title and name properties where appropriate. If all required
    * parameters of the resource are set, then the link should have a resolved URI as href property. If some or all
    * required parameters are null, a link with a URI template should be created instead.
-   * @return a {@link Link} instance where href, title and names properties are already set as required
+   * @return a {@link Link} instance where href, title and name properties are already set as required
    */
   @ResourceLink
   Link createLink();
