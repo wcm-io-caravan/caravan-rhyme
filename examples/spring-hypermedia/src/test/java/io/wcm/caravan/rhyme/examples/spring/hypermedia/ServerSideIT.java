9deb130b20cbab18615710b1d6eb794494108657
Steffen <steffen.sauder@gmail.com>
2021-09-29 08:54:31 +0200

    wording in comments

--- examples/spring-hypermedia/src/test/java/io/wcm/caravan/rhyme/examples/spring/hypermedia/ServerSideIT.java
+++ examples/spring-hypermedia/src/test/java/io/wcm/caravan/rhyme/examples/spring/hypermedia/ServerSideIT.java
@@ -3,14 +3,14 @@ package io.wcm.caravan.rhyme.examples.spring.hypermedia;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * This test verifies the API functionality when being used directly from within the same application.
+ * This test verifies the API functionality when being used from within the same application.
  * It executes all tests defined in {@link AbstractCompanyApiIT} directly against the server-side
  * resource implementations. It does not verify the link generation or JSON (de)serialization as the
  * {@link ClientSideIT} does.
  */
 public class ServerSideIT extends AbstractCompanyApiIT {
 
-  @Autowired // This will inject the {@link CompanyApiController} as it directly implements that interface
+  @Autowired // This will inject the {@link CompanyApiController} as it implements that interface
   private CompanyApi api;
 
   @Override
